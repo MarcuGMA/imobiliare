91d3b4359f81e9f72e342d743bf6d09364c95ca4
marcugma <marcugma@gmail.com>
2022-06-02 11:39:15 +0300

    homepage

--- script/script.js
+++ script/script.js
@@ -18,8 +18,8 @@ const swiper2 = new Swiper('#swiper__wrapper2', {
   spaceBetween: 30,
   // Navigation arrows
   navigation: {
-    nextEl: '#swiper-wrapper-next',
-    prevEl: '#swiper-wrapper-prev',
+    nextEl: '#swiper-wrapper-next1',
+    prevEl: '#swiper-wrapper-prev1',
   },
 });
 
